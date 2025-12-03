bf56bcf69c7d6d536b6e9a44d0dcea5333919142
khcho96 <khcho1492@gmail.com>
2025-12-04 05:50:08 +0900

    feat: main 메서드 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,14 @@
 package christmas;
 
+import christmas.controller.ChristmasController;
+import christmas.service.EventService;
+import christmas.service.MenuService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        EventService eventService = new EventService();
+        MenuService menuService = new MenuService();
+        ChristmasController christmasController = new ChristmasController(eventService, menuService);
+        christmasController.run();
     }
 }
