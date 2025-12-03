4a9b1ac3bd2f890d3ba1f39ca61232af2cea042f
khcho96 <khcho1492@gmail.com>
2025-12-04 06:46:02 +0900

    feat: 주문 메뉴 검증 및 총 주문 금액 계산 기능 구현

---
+++ src/main/java/christmas/domain/OrderedMenu.java
@@ -0,0 +1,61 @@
+package christmas.domain;
+
+import static christmas.constant.ErrorMessage.MENU_ERROR;
+import static christmas.constant.ErrorMessage.MENU_MAX_ERROR;
+import static christmas.constant.ErrorMessage.MENU_ONLY_BEVERAGE_ERROR;
+
+import christmas.constant.Menu;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public class OrderedMenu {
+
+    private final EnumMap<Menu, Integer> orderedMenu;
+
+    private OrderedMenu(EnumMap<Menu, Integer> orderedMenu) {
+        this.orderedMenu = orderedMenu;
+    }
+
+    public static void from(List<String> menuNames) {
+        EnumMap<Menu, Integer> orderedMenu = new EnumMap<>(Menu.class);
+        for (String menuName : menuNames) {
+            Menu menu = Menu.from(menuName);
+            validateExistence(menu);
+
+            orderedMenu.put(menu, orderedMenu.getOrDefault(menu, 0) + 1);
+        }
+
+        validateOnlyBeverage(orderedMenu);
+        validateCount(orderedMenu);
+    }
+
+    private static void validateExistence(Menu menu) {
+        if (menu.equals(Menu.NONE)) {
+            throw new IllegalArgumentException(MENU_ERROR.getErrorMessage());
+        }
+    }
+
+    private static void validateOnlyBeverage(EnumMap<Menu, Integer> orderedMenu) {
+        Set<Menu> menus = orderedMenu.keySet();
+        if (menus.stream().allMatch(Menu::isBeverage)) {
+            throw new IllegalArgumentException(MENU_ONLY_BEVERAGE_ERROR.getErrorMessage());
+        }
+    }
+
+    private static void validateCount(EnumMap<Menu, Integer> orderedMenu) {
+        Optional<Integer> sum = orderedMenu.values().stream().reduce(Integer::sum);
+        if (sum.isPresent() && sum.get() > 20) {
+            throw new IllegalArgumentException(MENU_MAX_ERROR.getErrorMessage());
+        }
+    }
+
+    public int calculateTotalPrice() {
+        int totalPrice = 0;
+        for (Menu menu : orderedMenu.keySet()) {
+            totalPrice = menu.getPrice() * orderedMenu.get(menu);
+        }
+        return totalPrice;
+    }
+}
