3372faea0313273ad461a455685279e065558d2e
khcho96 <khcho1492@gmail.com>
2025-12-04 05:57:16 +0900

    feat: 입력 기능 구현

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,21 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String DATE_REQUEST = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n"
+            + "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String MENU_REQUEST = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. "
+            + "(e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+
+    public static String readDate() {
+        System.out.println(DATE_REQUEST);
+        return Console.readLine();
+    }
+
+    public static String readMenu() {
+        System.out.println(MENU_REQUEST);
+        return Console.readLine();
+    }
+}
