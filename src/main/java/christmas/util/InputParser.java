d66cc6451bb0cc5d702fd3c2d05679bdc0291882
khcho96 <khcho1492@gmail.com>
2025-12-04 05:51:39 +0900

    feat: 입력값 처리 및 검증 기능 구현

---
+++ src/main/java/christmas/util/InputParser.java
@@ -0,0 +1,41 @@
+package christmas.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class InputParser {
+
+    private static final String FIRST_DELIMITER = ",";
+    private static final String SECOND_DELIMITER = "-";
+
+    private InputParser() {
+    }
+
+    public static Integer parseToInteger(String rawInput) {
+        Validator.validateDate(rawInput);
+        rawInput = rawInput.strip();
+
+        return NumberConvertor.convertToNumber(rawInput);
+    }
+
+    public static List<String> parseToElements(String rawInput) {
+        Validator.validateMenu(rawInput);
+
+        List<String> menuNames = new ArrayList<>();
+        String[] split = rawInput.split(FIRST_DELIMITER);
+        for (String s : split) {
+            String[] split1 = s.split(SECOND_DELIMITER);
+            String menuName = split1[0];
+            int count = NumberConvertor.convertToNumber(split1[1]);
+
+            Validator.validateMenuCount(count);
+            Validator.validateMenuUnique(menuNames, menuName);
+
+            for (int i = 0; i < count; i++) {
+                menuNames.add(menuName);
+            }
+        }
+
+        return menuNames;
+    }
+}
---
+++ src/main/java/christmas/util/Validator.java
@@ -0,0 +1,37 @@
+package christmas.util;
+
+import static christmas.constant.ErrorMessage.DATE_ERROR;
+import static christmas.constant.ErrorMessage.MENU_ERROR;
+
+import java.util.List;
+
+public final class Validator {
+
+    private static final String CSV_FORMAT = "^(\\S+-\\d+)+ *(, *(\\S+-\\d+)+ *)*,? *(\\S+-\\d+)+$";
+
+    private Validator() {}
+
+    public static void validateDate(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException(DATE_ERROR.getErrorMessage());
+        }
+    }
+
+    public static void validateMenu(String input) {
+        if (input == null || input.isBlank() || !input.strip().matches(CSV_FORMAT)) {
+            throw new IllegalArgumentException(MENU_ERROR.getErrorMessage());
+        }
+    }
+
+    public static void validateMenuCount(int count) {
+        if (count == 0) {
+            throw new IllegalArgumentException(MENU_ERROR.getErrorMessage());
+        }
+    }
+
+    public static void validateMenuUnique(List<String> menuNames, String menuName) {
+        if (menuNames.contains(menuName)) {
+            throw new IllegalArgumentException(MENU_ERROR.getErrorMessage());
+        }
+    }
+}
